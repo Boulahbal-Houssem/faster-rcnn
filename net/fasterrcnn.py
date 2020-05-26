af630d1ee5e50146a90cbd89f4d0c267a385dd12
Houssem BOULAHBAL <boulahbalhoussem@gmail.com>
2020-05-26 11:25:38 +0200

    update

--- net/fasterrcnn.py
+++ net/fasterrcnn.py
@@ -1,22 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
+import riopooling, rpn
 
-class RPN(tf.keras.layers.Layer):
-    def __init__(self):
-        super().__init__()
-    
-    def build(self, input):
-        pass
-
-    def call(self, trainable):
-        pass
 
 
 class FasterRCNN(tf.keras.Model):
-    def __init__(self, _input_shape, _nbr_classes, name="FasterRCNN", **kwargs):
+    def __init__(self, _input_shape,_anchor_num , name="FasterRCNN", **kwargs):
         super(FasterRCNN, self).__init__(name=name, **kwargs)
         self._input_shape = _input_shape
-        self._nbr_classes = _nbr_classes
         self.input_layer = Input(shape=_input_shape)
         self.feature_extractor = tf.keras.applications.ResNet50V2(
             include_top=False, weights='imagenet', input_tensor=Input(shape=_input_shape),
@@ -24,9 +15,9 @@ class FasterRCNN(tf.keras.Model):
         
         for layer in self.feature_extractor.layers:
             layer.trainable = False
+        self.RPN = RPN(_anchor_num)
+        self.riopooling = RoiPoolingConv(pool_size=7, num_rois=_anchor_num)
 
-        self.classifier = Dense(self._nbr_classes,activation="relu")
-        self.out = self.call(self.input_layer)
         super(FasterRCNN, self).__init__(
             inputs=self.input_layer,
             outputs=self.out,
--- net/rpn.py
+++ net/rpn.py
@@ -2,19 +2,19 @@ import tensorflow as tf
 from tensorflow.keras.layers import *
 
 class RPN(tf.keras.layers.Layer):
-    def __init__(self):
+    def __init__(self, num_anchors=1):
         super().__init__()
-        self.intermediate = Conv2D(filters=512,kernel_size=[3,3])
-        self.cls_conv = Conv2D(filters=2,kernel_size=[1,1])
-        self.box_reg_conv = Conv2D(filters=4,kernel_size=[1,1])       
+        self.intermediate = Conv2D(filters=512,kernel_size=[3, 3],kernel_initializer="normal",padding="same")
+        self.cls_conv = Conv2D(filters=num_anchors, kernel_size=[1,1],kernel_initializer='uniform',padding="same", name="RPN_cls_out",activation="softmax")
+        self.box_reg_conv = Conv2D(filters=num_anchors*4, kernel_size=[1,1],kernel_initializer='zero',padding="same", name="RPN_box_reg",activation="relu")       
     def call(self, input_tensor, training=False):
         x = self.intermediate(input_tensor)
         out_cls = self.cls_conv(x)
         out_box_reg = self.box_reg_conv(x)
         return [out_cls,out_box_reg]
 
-if __name__ == '__main__':
-    inputs = Input(shape=(222,222,3))
-    rpn_out = RPN()(inputs)
+if __name__ == "__main__":
+    inputs = Input(shape=(7,7,1048))
+    rpn_out = RPN(num_anchors=7)(inputs)
     model = tf.keras.Model(inputs=inputs, outputs=rpn_out, name="RPN TEST")
     model.summary()
